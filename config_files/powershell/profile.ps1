4e2589d6225c70b105e3cf4222dc411d8ac835d5
= <=>
2026-04-06 16:37:54 -0300

    Replace profile with correct PS7 one (prompt + PSReadLine)

--- config_files/powershell/profile.ps1
+++ config_files/powershell/profile.ps1
@@ -1,50 +1,89 @@
+# oh-my-posh init pwsh --config "~\OneDrive - Accenture\Documents\PowerShell\myposh.omp.json" | Invoke-Expression
 
+function prompt {
+    $timeColor = "`e[38;5;245m"  # Grey
+    $userColor = "`e[38;5;135m"  # Purple
+    $hostColor = "`e[0m@"
+    $pathColor = "`e[38;5;46m"   # Green
+    $reset = "`e[0m"
+
+    # Time, User and path
+    $timeDisplay = "$timeColor[" + (Get-Date -Format "HH:mm") + "]$reset"
+    $userDisplay = "$userColor" + "douglas" + "$reset"
+    $pathDisplay = "$pathColor$(Get-Location)$reset"
+
+    # Lambda symbol prompt
+    $lambdaSymbol = "`e[38;5;208mλ$reset"
+
+    "$timeDisplay $userDisplay $hostColor $pathDisplay `n$lambdaSymbol "
+}
+
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -EditMode Windows
 
 $ENV:EDITOR = "nvim"
 $ENV:VISUAL = "nvim-qt"
 $ENV:LANG = "pt_br.utf-8"
+$ENV:XDG_DATA_HOME = "C:\Users\douglas.f.silva\AppData\Local"
 
-set-alias l ls
-set-alias grep select-string
+set-alias grep rg 
 set-alias vi nvim
 set-alias vim nvim
 set-alias edit nvim
 set-alias grep select-string
-set-alias cat bat -Option AllScope
+set-alias cat bat
 Set-Alias reload Reload-Powershell
+Set-Alias top btop
+Set-Alias l ls
+
 ${function:~} = { Set-Location ~ }
 
 function winget-list {
     winget list --upgrade-available --source=winget
 }
 
+# Import-Module ~\scripts\list-calendar-events.ps1
+
 function which ($command) { 
     Get-Command -Name $command -ErrorAction SilentlyContinue | 
         Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue 
 }
 
-function external-display-only {
-    DisplaySwitch.exe /external
+function touch ($file) {
+    New-Item -Path $file -ItemType File -Force
 }
 
-function extend-notebook-display {
-    DisplaySwitch.exe /extend
+function new-symlink($source, $destiny) {
+    New-Item -ItemType SymbolicLink -Path $destiny -Target $source
 }
 
-function test-sound {
-    [System.Media.SystemSounds]::Beep.Play()
+function notes {
+    cd '~\OneDrive - Accenture\Notebooks\Accenture\'
 }
 
-function mode-high-performance {
-    powercfg -setactive 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
+Import-Module ~\scripts\my_ps1_functions.ps1
+
+function claude-yolo { claude --dangerously-skip-permissions --allowedTools "*" @args }
+
+function y {
+    $tmp = (New-TemporaryFile).FullName
+    yazi $args --cwd-file="$tmp"
+    $cwd = Get-Content -Path $tmp -Encoding UTF8
+    if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+        Set-Location -LiteralPath (Resolve-Path -LiteralPath $cwd).Path
+    }
+    Remove-Item -Path $tmp
 }
 
-function mode-power-saver {
-    powercfg -setactive a1841308-3541-4fab-bc81-f71556f20b4a
+
+function cal {
+    rusti-cal --color | bat
 }
 
-function mode-balanced {
-    powercfg -setactive 381b4222-f694-41f0-9685-ff5bb260df2e
+function tail {
+  param($Path, $n = 20, [switch]$f = $false)
+  Get-Content $Path -Tail $n -Wait:$f
 }
 
 # function FZE { fzf | % { nvim $_ }   }
@@ -64,6 +103,5 @@ function FZV {
     fzf --preview 'bat  --color=always {}'
 }
 
-
-
-Set-Location ~
+Import-Module -Name Microsoft.WinGet.CommandNotFound
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
